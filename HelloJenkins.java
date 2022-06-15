e985b382af389ee1e9192c273a812d38f26a53ad
smsree <103555247+smsree@users.noreply.github.com>
2022-06-15 12:46:10 +0530

    Add files via upload

---
+++ HelloJenkins.java
@@ -0,0 +1,10 @@
+package Jenkins_Demo;
+public class HelloJenkins {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello jenkins world by java");
+
+	}
+
+}
